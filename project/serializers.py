aca4f494dfc7c60ba053c169b62a5cde6385e47d
Acetonerer <bolokan2014@gmail.com>
2024-06-21 10:33:22 +0700

    Фикс get метода

--- project/serializers.py
+++ project/serializers.py
@@ -6,32 +6,19 @@ from project.models import Project
 
 class ProjectSerializer(serializers.ModelSerializer):
     main_group = serializers.SerializerMethodField()
-    groups = serializers.SerializerMethodField()
 
     class Meta:
         model = Project
-        fields = ['amo_id', 'name', 'main_group', 'groups']
+        fields = ['amo_id', 'name', 'main_group']
 
     def get_main_group(self, obj):
         main_group = obj.main_group
         if main_group:
-            main_group_serializer = GroupSerializer(main_group)
-            return main_group_serializer.data
+            return self._serialize_group_with_subgroups(main_group)
         return None
 
-    def get_groups(self, obj):
-        main_group = obj.main_group
-        all_groups = obj.groups.all()
-
-        def get_subgroups(group):
-            subgroups = group.subgroups.all()
-            return GroupSerializer(subgroups, many=True).data
-
-        project_groups = []
-        for group in all_groups:
-            if group.parent_group == main_group:
-                group_data = GroupSerializer(group).data
-                group_data['subgroups'] = get_subgroups(group)
-                project_groups.append(group_data)
-
-        return project_groups
+    def _serialize_group_with_subgroups(self, group):
+        group_data = GroupSerializer(group).data
+        subgroups = group.subgroups.all()
+        group_data['subgroups'] = [self._serialize_group_with_subgroups(subgroup) for subgroup in subgroups]
+        return group_data
