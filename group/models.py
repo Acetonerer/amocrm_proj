e7bdd2d03589a893ad9261e33327d1824dd81bf9
Acetonerer <bolokan2014@gmail.com>
2024-06-27 09:32:01 +0700

    Фикс

--- group/models.py
+++ group/models.py
@@ -6,7 +6,7 @@ class Group(models.Model):
     name = models.CharField(max_length=100, default='')
     parent_group = models.ForeignKey('self', null=True, blank=True, on_delete=models.CASCADE, related_name='subgroups')
     project = models.ForeignKey(Project, on_delete=models.CASCADE, related_name='groups')
-    result = models.CharField(max_length=100)
+    result = models.CharField(max_length=100, default='')
 
     def __str__(self):
         return self.name
